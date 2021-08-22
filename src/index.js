b857f0d77ddbb63c2f33609da89392238127cfab
Animesh-Kr13 <kumar.animesh2759@gmail.com>
2021-08-22 12:37:09 +0530

    inor update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {ContextProvider} from "./Context"
 
 ReactDOM.render(
@@ -11,5 +10,3 @@ ReactDOM.render(
   </ContextProvider>,
   document.getElementById('root')
 );
-
-reportWebVitals();
